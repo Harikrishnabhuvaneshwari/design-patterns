90bf0bbe6bd26e49b352575dddf175fc40afa663
Hari Krishna <harikrishnabhuvaneshwari@yahoo.com>
2023-03-06 21:13:29 +0530

    builder design pattern

---
+++ src/main/java/com/dp/creational/builder/TestBuilderPattern.java
@@ -0,0 +1,17 @@
+package src.main.java.com.dp.creational.builder;
+
+public class TestBuilderPattern {
+    public static void main(String[] args) {
+        User user1 = new User.UserBuilder("Hari Krishna", "Prakash").build();
+
+        User user2 = new User.UserBuilder("Ram", "Kumar")
+                .age(23)
+                .salary(1500000)
+                .address("Shoolagiri")
+                .mobileNo("8094567390")
+                .build();
+
+        System.out.println(user1);
+        System.out.println(user2);
+    }
+}
---
+++ src/main/java/com/dp/creational/builder/User.java
@@ -0,0 +1,73 @@
+package src.main.java.com.dp.creational.builder;
+
+import lombok.Getter;
+
+@Getter
+public class User {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final long salary;
+    private final String address;
+    private final String mobileNo;
+
+    private User(UserBuilder userBuilder) {
+        this.firstName = userBuilder.firstName;
+        this.lastName = userBuilder.lastName;
+        this.age = userBuilder.age;
+        this.salary = userBuilder.salary;
+        this.address = userBuilder.address;
+        this.mobileNo = userBuilder.mobileNo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                ", mobileNo='" + mobileNo + '\'' +
+                '}';
+    }
+
+    protected static class UserBuilder {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private long salary;
+        private String address;
+        private String mobileNo;
+
+        UserBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        UserBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        UserBuilder salary(long salary) {
+            this.salary = salary;
+            return this;
+        }
+
+        UserBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        UserBuilder mobileNo(String mobileNo) {
+            this.mobileNo = mobileNo;
+            return this;
+        }
+
+        User build() {
+            User user = new User(this);
+            return user;
+        }
+    }
+}
